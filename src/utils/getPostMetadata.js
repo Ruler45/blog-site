21d5f6a5f490c302e0035bfa0890997414e24e50
Sahin Alam <sahin0945@gmail.com>
2024-10-20 14:57:29 +0530

    rebase test

--- src/utils/getPostMetadata.js
+++ src/utils/getPostMetadata.js
@@ -4,13 +4,13 @@ import matter from "gray-matter"; // g
 
 
 const getPostMetadata = () => {
-    // The path to the folder containing the markdown files
-  const folder = "src/content/posts";
-  const files = fs.readdirSync(path.join(process.cwd(), folder));
+    // The path to the folderName containing the markdown files
+  const folderName = "src/content/posts";
+  const files = fs.readdirSync(path.join(process.cwd(), folderName));
   const markdownFiles = files.filter((file) => file.endsWith(".md"));
   // Read the content of each markdown file
   const posts = markdownFiles.map((filename) => {
-    const filePath = `${folder}/${filename}`;
+    const filePath = `${folderName}/${filename}`;
     const fileContent = fs.readFileSync(filePath, "utf8");
     const { data } = matter(fileContent);
     // console.log(data);
