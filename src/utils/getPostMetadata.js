b6b51e5f540939cd854cbc11351a61304410c557
Sahin Alam <sahin0945@gmail.com>
2024-10-20 14:55:56 +0530

    merge test

--- src/utils/getPostMetadata.js
+++ src/utils/getPostMetadata.js
@@ -1,16 +1,16 @@
 import fs from "fs"; // fs is a Node.js module that provides file system-related functionality
 import path from "path"; // path is a Node.js module that provides utilities for working with file and directory paths
-import matter from "gray-matter"; // gray-matter is a package that helps to parse frontmatter from markdown files
+import matter from "gray-matter"; // g
 
 
 const getPostMetadata = () => {
     // The path to the folder containing the markdown files
-  const foldereName = "src/content/posts";
-  const files = fs.readdirSync(path.join(process.cwd(), foldereName));
+  const folder = "src/content/posts";
+  const files = fs.readdirSync(path.join(process.cwd(), folder));
   const markdownFiles = files.filter((file) => file.endsWith(".md"));
   // Read the content of each markdown file
   const posts = markdownFiles.map((filename) => {
-    const filePath = `${foldereName}/${filename}`;
+    const filePath = `${folder}/${filename}`;
     const fileContent = fs.readFileSync(filePath, "utf8");
     const { data } = matter(fileContent);
     // console.log(data);
